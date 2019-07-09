f85327e507cece2f97ff43ad5a012d685340415e
unknown <pamunkasaji>
2019-07-09 18:39:33 +0700

    a

--- application/libraries/pdf.php
+++ application/libraries/pdf.php
@@ -8,10 +8,29 @@ use Dompdf\Dompdf;
 
 class Pdf extends Dompdf
 {
- public function __construct()
- {
-   parent::__construct();
- } 
+  protected $ci;
+
+  public function __construct()
+  {
+        parent::__construct();
+        $this->ci =& get_instance();
+  }
+
+  public function generate($view, $data = array(), $filename = 'Laporan', $paper = 'A4', $orientation='portrait')
+  {
+    $dompdf = new Dompdf();
+    $html = $this->ci->load->view($view, $data, TRUE);
+    $dompdf->loadHtml($html);
+    $dompdf->setPaper($paper, $orientation);
+    // Render the HTML as PDF
+    $dompdf->render();
+      $dompdf->stream( $filename . ".pdf", array("Attachment" => FALSE));
+
+  }
+
 }
 
 ?>
+
+
+
