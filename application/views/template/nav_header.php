8f7f5f09d71f787aed28d187e74615b688b20319
unknown <pamunkasaji>
2019-07-12 09:55:59 +0700

    minor

--- application/views/template/nav_header.php
+++ application/views/template/nav_header.php
@@ -54,117 +54,135 @@ elseif(!empty($session[0]['level']))
 							<a class="collapse-item" href="<?php echo site_url('Area'); ?>"><i class="icon-angle-right"></i> Area </a>
 							<a class="collapse-item" href="<?php echo site_url('Subarea'); ?>"><i class="icon-angle-right"></i> Subarea </a>
 							<a class="collapse-item" href="<?php echo site_url('Kodeqr'); ?>"><i class="icon-angle-right"></i> Kodeqr </a>
+							<a class="collapse-item" href="<?php echo site_url('Material'); ?>"><i class="icon-angle-right"></i> Material </a>
+							<a class="collapse-item" href="<?php echo site_url('Standard'); ?>"><i class="icon-angle-right"></i> Standard </a>
 
 						</div>
 					</div>
 				</li>
 
-				<?php
-			}
-				//Jika  Login sebagai Owner
-			elseif($session['level'] =='supervisor')
-			{
-				?>
-
-				<!-- Heading -->
-				<div class="sidebar-heading">
-					SUPERVISOR
-				</div>
-
-				<!-- Nav Item - Pages Collapse Menu -->
 				<li class="nav-item active">
-					<a class="nav-link" href="<?php echo site_url('Dashboard'); ?>"> <i class="fas fa-fw fa-table"></i> <span>Dashboard Harian</span></a>
-					<a class="nav-link" href="<?php echo site_url('DashboardBulanan'); ?>"> <i class="fas fa-fw fa-table"></i> <span>Dashboard Bulanan</span></a>
-				</li>
-				<?php
-			}
-			?>
-			<!-- Nav Item - Tables -->
-			<li class="nav-item active">
-				<a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">
-					<i class="fas fa-sign-out-alt"></i>
-					<span>Logout</span></a>
-				</li>
+					<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLaporan" aria-expanded="true" aria-controls="collapsePages">
+						<i class="fas fa-fw fa-folder"></i>
+						<span>Data Laporan</span>
+					</a>
+					<div id="collapseLaporan" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+						<div class="bg-white py-2 collapse-inner rounded">
+							<h6 class="collapse-header">Laporan</h6>
 
-				<!-- Divider -->
-				<hr class="sidebar-divider d-none d-md-block">
+							<a class="collapse-item" href="<?php echo site_url('LaporanSCA'); ?>"><i class="icon-angle-right"></i> Laporan SCA </a>
+							<a class="collapse-item" href="<?php echo site_url('#'); ?>"><i class="icon-angle-right"></i> Lost and Found </a>
+							<a class="collapse-item" href="<?php echo site_url('#'); ?>"><i class="icon-angle-right"></i> Kerusakan </a>
 
-				<!-- Sidebar Toggler (Sidebar) -->
-				<div class="text-center d-none d-md-inline">
-					<button class="rounded-circle border-0" id="sidebarToggle"></button>
-				</div>
-
-			</ul>
-			<!-- End of Sidebar -->
-
-			<!-- Content Wrapper -->
-			<div id="content-wrapper" class="d-flex flex-column">
-
-				<!-- Main Content -->
-				<div id="content">
-
-					<!-- Topbar -->
-					<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-						<!-- Sidebar Toggle (Topbar) -->
-						<button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-							<i class="fa fa-bars"></i>
-						</button>
-
-
-						<!-- Topbar Navbar -->
-						<ul class="navbar-nav ml-auto">
-
-							<!-- Nav Item - User Information -->
-							<li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $session['username']; ?></span>
-                <img class="img-profile rounded-circle" src="<?php echo base_url().'/gambar/'.$logo;?>">
-              </a>
-              <!-- Dropdown - User Information -->
-              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="<?php echo base_url().'Profile/tambahGambar/'.$session['id_karyawan'];?>">
-                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Profile
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Settings
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Activity Log
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Logout
-                </a>
-              </div>
-            </li>
-							<!-- Dropdown - User Information -->
-
-
-					</ul>
-
-				</nav>
-				<!-- End of Topbar -->
-
-				<!-- Logout Modal-->
-				<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-					<div class="modal-dialog" role="document">
-						<div class="modal-content">
-							<div class="modal-header">
-								<h5 class="modal-title" id="exampleModalLabel">Keluar Dari Aplikasi</h5>
-								<button class="close" type="button" data-dismiss="modal" aria-label="Close">
-									<span aria-hidden="true">×</span>
-								</button>
-							</div>
-							<div class="modal-body">Tekan Tombol "Logout" untuk mengakhiri sesi</div>
-							<div class="modal-footer">
-								<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-								<a class="btn btn-primary" href="<?php echo base_url('Login/keluar'); ?>">Logout</a>
-							</div>
 						</div>
 					</div>
-				</div>
+
+					<?php
+				}
+				//Jika  Login sebagai Owner
+				elseif($session['level'] =='supervisor')
+				{
+					?>
+
+					<!-- Heading -->
+					<div class="sidebar-heading">
+						SUPERVISOR
+					</div>
+
+					<!-- Nav Item - Pages Collapse Menu -->
+					<li class="nav-item active">
+						<a class="nav-link" href="<?php echo site_url('Dashboard'); ?>"> <i class="fas fa-fw fa-table"></i> <span>Dashboard Harian</span></a>
+						<a class="nav-link" href="<?php echo site_url('DashboardBulanan'); ?>"> <i class="fas fa-fw fa-table"></i> <span>Dashboard Bulanan</span></a>
+					</li>
+					<?php
+				}
+				?>
+				<!-- Nav Item - Tables -->
+				<li class="nav-item active">
+					<a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">
+						<i class="fas fa-sign-out-alt"></i>
+						<span>Logout</span></a>
+					</li>
+
+					<!-- Divider -->
+					<hr class="sidebar-divider d-none d-md-block">
+
+					<!-- Sidebar Toggler (Sidebar) -->
+					<div class="text-center d-none d-md-inline">
+						<button class="rounded-circle border-0" id="sidebarToggle"></button>
+					</div>
+
+				</ul>
+				<!-- End of Sidebar -->
+
+				<!-- Content Wrapper -->
+				<div id="content-wrapper" class="d-flex flex-column">
+
+					<!-- Main Content -->
+					<div id="content">
+
+						<!-- Topbar -->
+						<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+							<!-- Sidebar Toggle (Topbar) -->
+							<button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+								<i class="fa fa-bars"></i>
+							</button>
+
+
+							<!-- Topbar Navbar -->
+							<ul class="navbar-nav ml-auto">
+
+								<!-- Nav Item - User Information -->
+								<li class="nav-item dropdown no-arrow">
+									<a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $session['username']; ?></span>
+										<img class="img-profile rounded-circle" src="<?php echo base_url().'/gambar/'.$logo;?>">
+									</a>
+									<!-- Dropdown - User Information -->
+									<div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+										<a class="dropdown-item" href="<?php echo base_url().'Profile/tambahGambar/'.$session['id_karyawan'];?>">
+											<i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+											Profile
+										</a>
+										<a class="dropdown-item" href="#">
+											<i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+											Settings
+										</a>
+										<a class="dropdown-item" href="#">
+											<i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+											Activity Log
+										</a>
+										<div class="dropdown-divider"></div>
+										<a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+											<i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+											Logout
+										</a>
+									</div>
+								</li>
+								<!-- Dropdown - User Information -->
+
+
+							</ul>
+
+						</nav>
+						<!-- End of Topbar -->
+
+						<!-- Logout Modal-->
+						<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+							<div class="modal-dialog" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5 class="modal-title" id="exampleModalLabel">Keluar Dari Aplikasi</h5>
+										<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+											<span aria-hidden="true">×</span>
+										</button>
+									</div>
+									<div class="modal-body">Tekan Tombol "Logout" untuk mengakhiri sesi</div>
+									<div class="modal-footer">
+										<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+										<a class="btn btn-primary" href="<?php echo base_url('Login/keluar'); ?>">Logout</a>
+									</div>
+								</div>
+							</div>
+						</div>
