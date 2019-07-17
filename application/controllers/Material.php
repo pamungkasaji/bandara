88b2295a74d240d854845602c690717b08cf4895
pamungkasaji <pamungkasaji1997@gmail.com>
2019-07-17 08:22:27 +0700

    material

---
+++ application/controllers/Material.php
@@ -0,0 +1,130 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Material extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('material_model');
+		$this->load->model('login_m');
+		$this->load->model('subarea_model');
+		$this->load->model('Navbar_model');
+		if(!$this->session->userdata('id_karyawan'))
+		{
+			redirect('Login');
+		}
+		$this->load->helper(array('form', 'url','download'));
+	}
+	public function index()
+	{
+		$data['session']	= $this->session->all_userdata();
+		$data['material'] = $this->material_model->getMaterial();
+	  $data['logo'] = $this->login_m->ambil_gambar($this->session->userdata('id_karyawan'));
+	    //include head, header, footer di view dihapus dulu
+	    //parameter $data tidak diubah, ikut controller bersangkutan,
+	    //kalo parameter $nav sama di semua controller
+	  $this->Navbar_model->view_loader('MaterialList', $data);
+	}
+	//function tambahMaterial
+	public function tambahMaterial()
+	{
+		$data['logo'] = $this->login_m->ambil_gambar($this->session->userdata('id_karyawan'));
+		$data['session']	= $this->session->all_userdata();
+		$data['subarea'] 		= $this->subarea_model->getSubarea(); 
+		$this->Navbar_model->view_loader('MaterialForm', $data);
+	}
+	//function input data
+	public function simpanMaterial()
+	{
+		$data['logo'] = $this->login_m->ambil_gambar($this->session->userdata('id_karyawan'));
+		$data['session']	= $this->session->all_userdata();
+		$data['subarea'] 		= $this->subarea_model->getSubarea(); 
+		//Untuk Validasi
+		//query simpan data Material
+		if($this->material_model->simpanMaterial())
+		{
+			//load notifikasi sukses
+			$data['sukses']  = '
+					<div class="alert alert-success"><p><strong>Input Data Material Sukses</strong></p></div>';
+					$this->Navbar_model->view_loader('MaterialForm', $data);
+		}
+		else
+		{
+			//load notifikasi gagal
+			$data['error']  = '
+							<div class="msg msg-error">
+								<p><strong>Input Material Data Gagal!</strong></p>
+							</div>';
+					$this->Navbar_model->view_loader('MaterialForm', $data);
+		}
+	}
+	//ubah
+	public function ubah()
+	{
+		$data['session']	= $this->session->all_userdata();
+		$data['logo'] = $this->login_m->ambil_gambar($this->session->userdata('id_karyawan'));
+		$id_material		= $this->input->get('id_material');
+		//$data['MTR']= $this->material_model->getMTRUpdate($kode); 
+		$data['subarea'] 		= $this->subarea_model->getSubarea(); 
+		$data['MTR']		= $this->material_model->getMTRUpdate($id_material);
+		$data['material']		= $this->material_model->getMaterialUpdate($id_material);
+		$this->Navbar_model->view_loader('MaterialForm', $data);
+	}
+	public function prosesUbah()
+	{
+		$data['session']	= $this->session->all_userdata();
+		$data['logo'] = $this->login_m->ambil_gambar($this->session->userdata('id_karyawan'));
+		$id_material	= $this->input->get('id_material');
+		//$data['MTR']= $this->material_model->getMTRUpdate($kode); 
+		//Jika update data sukses
+		if($this->material_model->ubah())
+		{
+			//load notifikasi sukses
+			$data['sukses']= '
+							<div class="alert alert-success">
+								<p><strong>Update Data Material Sukses</strong></p>
+							</div>';
+							$data['material'] = $this->material_model->getMaterial();
+			$this->Navbar_model->view_loader('MaterialList', $data);
+		}
+		//Jika update data tidak sukses
+		else
+		{
+			//load notifikasi gagal
+			$data['error'] = '
+								<div class="msg msg-error"><p><strong>Update Data Material Gagal!</strong></p>
+								</div>';
+								$data['material'] = $this->material_model->getMaterial();
+			$this->Navbar_model->view_loader('MaterialList', $data);
+		}
+	}
+	//function hapus
+	public function hapus()
+	{
+		$data['session']	= $this->session->all_userdata();
+		$id_material		= $this->input->get('id_material');
+		$data['logo'] = $this->login_m->ambil_gambar($this->session->userdata('id_karyawan'));
+		//panggil query hapus di model
+		if($this->material_model->hapus($id_material))
+		{
+			//load notifikasi sukses
+			$data['sukses']= '
+							<div class="alert alert-success">
+								<p><strong>Hapus Data Material Sukses</strong></p>
+							</div>';
+							$data['material'] = $this->material_model->getMaterial();
+			$this->Navbar_model->view_loader('MaterialList', $data);
+		}
+		//Jika update data tidak sukses
+		else
+		{
+			//load notifikasi gagal
+			$data['error'] = '
+								<div class="msg msg-error"><p><strong>Hapus Data Material Gagal!</strong></p>
+								</div>';
+								$data['material'] = $this->material_model->getMaterial();
+			$this->Navbar_model->view_loader('MaterialList', $data);
+		}
+	}
+}
+?>
---
+++ application/views/MaterialForm.php
@@ -0,0 +1,129 @@
+
+</head>
+
+<!--PAGE CONTENT -->
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+	<div class="card shadow mb-4">
+		<div class="card-header py-3">
+			<?php
+			//Form ini bisa dipakai untuk menambah ataupun edit Material
+			if(!empty($material[0]['id_material']))
+			{
+				?> <h6 class="m-0 font-weight-bold text-primary">Ubah Data Material</h6>
+				<?php
+			}
+			else
+			{
+				?> <h6 class="m-0 font-weight-bold text-primary">Tambah Data Material</h6>
+				<?php
+			}
+			?>
+
+		</div>
+
+		<div class="card-body">
+
+			<?php
+			//Jika update Sukses tampilkan Notifikasi sukses
+			if(@$sukses):
+				echo $sukses;
+			//Jika update gagal tampilkan Notifikasi error
+			else:
+				if(@$error){echo @$error;}
+			endif;
+			//Tampilkan Error Validasi
+			echo validation_errors();
+			//Jika tombol Simpan ditekan, maka jalankan controller Material->function simpan untuk menambah data atau function prosesUbah untuk mengubah data
+			if(!empty($material[0]['id_material']))
+			{
+				echo form_open_multipart('Material/prosesUbah');
+			}
+			else
+			{
+				echo form_open_multipart('Material/simpanMaterial');
+			}
+			?>
+
+			<input class="form-control" type="hidden" name="id_material" value="<?php echo isset($material[0]['id_material'])?$material[0]['id_material']:'';?>" >
+
+			<div class="form-group">
+				<label>Nama Material</label>
+				<input class="form-control" required name="nama_material" value="<?php echo isset($material[0]['nama_material'])?$material[0]['nama_material']:'';?>">
+			</div>
+			<div class="form-group">
+				<label>Subarea</label>
+				<?php
+				if(!empty($MTR))
+				{	
+					$no=0;
+					foreach($MTR as $detail)
+					{
+						$id_material=$detail['id_material'];
+						$nama_subarea=$detail['nama_subarea'];
+						$id_subarea=$detail['id_subarea'];
+
+						?>	
+						<SELECT name="id_subarea" required class="form-control">
+							<OPTION value="<?php echo isset($MTR[$no]['id_subarea'])?$MTR[$no]['id_subarea']:'';?>"><?php echo isset($MTR[$no]['nama_subarea'])?$MTR[$no]['nama_subarea']:'';?></OPTION>
+							<?php
+							if(!empty($subarea))
+							{
+								foreach($subarea as $data)
+								{
+									$id_subarea=$data['id_subarea'];
+									$nama_subarea=$data['nama_subarea'];
+
+									?>	
+
+									<OPTION value="<?php echo $id_subarea;?>"><?php echo $nama_subarea;?></OPTION>
+									<?php
+								}
+							}
+							?>		
+						</SELECT>
+						<?php
+						$no=$no+1;
+					}
+					?>
+					<?php
+				}
+				else
+				{
+					?>
+					<SELECT name="id_subarea" required class="form-control">
+						<OPTION value="<?php echo isset($MTR[0]['id_subarea'])?$MTR[0]['id_subarea']:'';?>"><?php echo isset($MTR[0]['nama_subarea'])?$MTR[0]['nama_subarea']:'';?></OPTION>
+						<?php
+						if(!empty($subarea))
+						{
+							foreach($subarea as $mtr)
+							{
+								$id_subarea=$mtr['id_subarea'];
+								$nama_subarea=$mtr['nama_subarea'];
+								?>	
+
+								<OPTION value="<?php echo $id_subarea;?>"><?php echo $nama_subarea;?></OPTION>
+								<?php
+							}
+						}
+						?>				       
+					</SELECT>
+					<?php
+				}
+				?>
+			</div>
+
+			<button type="submit" class="btn btn-success" name="save">Simpan</button>
+			<a href="<?php echo site_url('Material'); ?>"> <button type="button" class="btn btn-danger" name="batal">Kembali</button></a>
+
+		</div>
+
+
+		<!-- /.row -->
+	</div>
+
+</div>
+<!--END PAGE CONTENT -->
+
+</html>
